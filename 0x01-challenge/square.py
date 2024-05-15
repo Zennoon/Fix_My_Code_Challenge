5793e504995d226e649c508ff2dac2fc6690e789
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-05-15 16:59:27 +0000

    Fix pycodestyle issues

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
+"""
+Contains a square class
+"""
+
 
 class Square():
-    
+    """
+    Represents a real-life Square (but doesn't necessitate all sides 4 sides
+    equal)
+    """
     width = 0
+    height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """Initializes a new instance"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
+        """Calculates area"""
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        return (self.width * 4)
+    def permiter_of_my_square(self):
+        """Calculates perimeter"""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Informal string representation of the instance"""
         return "{}/{}".format(self.width, self.width)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
